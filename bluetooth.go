e63f6e7a6497764b95eed12dae8ebd9338604c25
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-08-06 20:11:19 +0200

    Clean up the code
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- bluetooth.go
+++ bluetooth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"fmt"
 	"strings"
 	"time"
 
@@ -12,19 +13,30 @@ import (
 	"golang.org/x/net/context"
 )
 
+const (
+	CharPersonShort  = "8a82"                                 // person data
+	CharWeightShort  = "8a21"                                 // weight data
+	CharBodyShort    = "8a22"                                 // body data
+	CharCommandShort = "8a81"                                 // command register
+	CharPerson       = "00008a82-0000-1000-8000-00805f9b34fb" // person data
+	CharWeight       = "00008a21-0000-1000-8000-00805f9b34fb" // weight data
+	CharBody         = "00008a22-0000-1000-8000-00805f9b34fb" // body data
+	CharCommand      = "00008a81-0000-1000-8000-00805f9b34fb" // command register
+)
+
 func showError(err error) {
 	if err == nil {
 		return
 	}
 
-	log.Errorf("[BLUETOOTH] Error: %s", err)
+	log.Errorf("[BLUETOOTH] error: %s", err)
 }
 
 // StartBluetooth runs the bluetooth cycle forever, scanning for some time and processing results
-func StartBluetooth() { //nolint:funlen
+func StartBluetooth() {
 	d, err := dev.NewDevice(config.Device)
 	if err != nil {
-		log.Fatalf("[BLUETOOTH] Can't use device: %s", err)
+		log.Fatalf("[BLUETOOTH] can't use device: %s", err)
 	}
 
 	ble.SetDefaultDevice(d)
@@ -34,67 +46,79 @@ func StartBluetooth() { //nolint:funlen
 	}
 
 	for {
-		log.Infoln("[BLUETOOTH] Starting scan...")
-
-		ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), config.ScanDuration.AsTimeDuration()))
-
-		cln, err := ble.Connect(ctx, filter)
-		if err != nil {
-			log.Warnf("[BLUETOOTH] Scan timeout: %s", err)
-			continue
+		if err := scan(filter); err != nil {
+			log.Warnf("[BLUETOOTH] %s", err)
 		}
-
-		// Normally, the connection is disconnected by us after our exploration.
-		// However, it can be asynchronously disconnected by the remote peripheral.
-		// So we wait(detect) the disconnection in the go routine.
-		go func(cln ble.Client) {
-			select {
-			case <-cln.Disconnected():
-				log.Infof("[BLUETOOTH] [ %s ] is disconnected ", cln.Addr())
-			case <-time.After(config.Sub.AsTimeDuration()):
-				log.Infof("[BLUETOOTH] [ %s ] timed out", cln.Addr())
-			}
-		}(cln)
-
-		log.Infof("[BLUETOOTH] [ %s ] is connected ...", cln.Addr())
-		log.Infoln("[BLUETOOTH] Discovering profile...")
-
-		p, err := cln.DiscoverProfile(true)
-		if err != nil {
-			log.Errorf("[BLUETOOTH] can't discover profile: %s", err)
-			showError(cln.CancelConnection())
-
-			continue
-		}
-
-		log.Infof("[BLUETOOTH] Name: '%s'", cln.Name())
-
-		// Start the exploration.
-		explore(cln, p)
-
-		log.Infof("[BLUETOOTH] Discovery done, waiting %f seconds before disconnecting.", config.Sub.AsTimeDuration().Seconds())
-		time.Sleep(config.Sub.AsTimeDuration())
-
-		// Disconnect the connection. (On OS X, this might take a while.)
-		log.Infof("[BLUETOOTH] Disconnecting [ %s ]... (this might take up to few seconds on OS X)", cln.Addr())
-
-		showError(cln.ClearSubscriptions())
-		showError(cln.CancelConnection())
 
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func explore(cln ble.Client, p *ble.Profile) {
+func scan(filter ble.AdvFilter) error {
+	log.Infoln("[BLUETOOTH] starting scan...")
+
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), config.ScanDuration.AsTimeDuration()))
+
+	log.Infoln("[BLUETOOTH] connecting...")
+
+	cln, err := ble.Connect(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("scan timeout: %w", err)
+	}
+
+	defer disconnect(cln)
+
+	// Normally, the connection is disconnected by us after our exploration.
+	// However, it can be asynchronously disconnected by the remote peripheral.
+	// So we wait(detect) the disconnection in the go routine.
+	go func(cln ble.Client) {
+		select {
+		case <-cln.Disconnected():
+			log.Infof("[BLUETOOTH] [ %s ] is disconnected ", cln.Addr())
+		case <-time.After(config.Sub.AsTimeDuration()):
+			log.Infof("[BLUETOOTH] [ %s ] timed out", cln.Addr())
+		}
+	}(cln)
+
+	log.Infof("[BLUETOOTH] [ %s ] is connected ...", cln.Addr())
+	log.Infoln("[BLUETOOTH] discovering profile...")
+
+	p, err := cln.DiscoverProfile(true)
+	if err != nil {
+		return fmt.Errorf("can't discover profile: %w", err)
+	}
+
+	log.Infof("[BLUETOOTH] address: '%s'; name: '%s'", cln.Addr(), cln.Name())
+
+	// Start the exploration.
+	showError(explore(cln, p))
+
+	log.Infof("[BLUETOOTH] discovery done, waiting %.0f seconds before disconnecting.", config.Sub.AsTimeDuration().Seconds())
+	time.Sleep(config.Sub.AsTimeDuration())
+
+	return nil
+}
+
+func disconnect(cln ble.Client) {
+	// Disconnect the connection. (On OS X, this might take a while.)
+	log.Infof("[BLUETOOTH] disconnecting [ %s ]...", cln.Addr())
+
+	showError(cln.ClearSubscriptions())
+	showError(cln.CancelConnection())
+
+	log.Infof("[BLUETOOTH] disconnected!")
+}
+
+func explore(cln ble.Client, p *ble.Profile) error {
 	// First we subscribe to the metrics
 	for _, s := range p.Services {
 		for _, c := range s.Characteristics {
 			switch c.UUID.String() {
-			case "8a21", "8a22", "8a82":
+			case CharWeightShort, CharBodyShort, CharPersonShort:
 				h := func(req []byte) { go decodeData(req) }
 
 				if err := cln.Subscribe(c, true, h); err != nil {
-					log.Errorf("[BLUETOOTH] subscribe failed: %s", err)
+					return fmt.Errorf("subscribe failed: %w", err)
 				}
 			}
 		}
@@ -103,19 +127,21 @@ func explore(cln ble.Client, p *ble.Profile) {
 	// Then we send the current time (which triggers data transmission)
 	for _, s := range p.Services {
 		for _, c := range s.Characteristics {
-			if c.UUID.String() == "8a81" {
-				log.Infof("[BLUETOOTH] Sending the time... ")
+			if c.UUID.String() == CharCommandShort {
+				log.Infof("[BLUETOOTH] sending the time... ")
 
 				binarytime := generateTime()
 
 				if err := cln.WriteCharacteristic(c, binarytime, true); err != nil {
-					log.Errorf("[BLUETOOTH] Error while writing command: %+v", err)
+					return fmt.Errorf("error while writing command: %w", err)
 				}
 
 				log.Infof("[BLUETOOTH] done.")
 			}
 		}
 	}
+
+	return nil
 }
 
 func generateTime() []byte {
